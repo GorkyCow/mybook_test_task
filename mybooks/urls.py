a32f2c764d8213a6c9acdf95dc6adfddad24eea8
Vitaly Kuznetsov <vkuznetsov@team.amocrm.com>
2019-04-25 01:26:26 +0300

    Add templates, login frame, page view frame

--- mybooks/urls.py
+++ mybooks/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('books_list/', include('mybooks_view.urls')),
+    path('', RedirectView.as_view(url='/books_list/', permanent=True)),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mybooks_view/views.py
+++ mybooks_view/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+#@login_required
+class BookListView(generic.ListView):
+    #model = Book
+
+    def get_queryset(self):
+        pass
+
+
+@login_required
+def books_list(request):
+    """
+    Presents list of books from https://mybook.ru/api/bookuserlist/
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # Метод 'all()' применен по умолчанию.
+    
+    # Отрисовка HTML-шаблона index.html с данными внутри 
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
